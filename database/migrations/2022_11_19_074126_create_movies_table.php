8d02ccb28f225744084847dd4fff9d43da656d6a
SiahXinYing <66939943+SiahXinYing@users.noreply.github.com>
2022-11-19 15:42:25 +0800

    database

---
+++ database/migrations/2022_11_19_074126_create_movies_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->Date('releasedDate');
+            $table->string('image');
+            $table->string('casts');
+            $table->string('synopsis');
+            $table->string('distributor');
+            $table->string('available');
+            $table->string('duration');
+            $table->foreignId('categoryID')->references('id')->on('categories')->onDelete('cascade');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
