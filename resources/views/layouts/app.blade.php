0e840767fe922ea68b69e2fc4093f2bc63a66893
HuiHuiKoh <kohhh-wm19@student.tarc.edu.my>
2022-11-19 18:50:47 +0800

    design update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,30 +42,35 @@
                 <nav id="navbar" class="navbar order-last order-lg-0">
                     <ul>
                         <li><a href="{{asset ('home') }}">Home</a></li>
-                        <li><a href="{{asset ('showtimes') }}">Showtimes</a></li>
-                        <li><a href="{{asset ('f&b') }}">F & B</a></li>
+                        <li class="dropdown"><a href="#"><span>Movies</span> <i class="bi bi-chevron-down"></i></a>
+                            <ul>
+                                <li><a href="{{asset ('showtimes') }}">Showtimes</a></li>
+                                <li><a href="{{asset ('f&b') }}">F & B</a></li>
+                            </ul>
+                        </li>
                         <li><a href="{{asset ('membership') }}">Membership</a></li>
                         <li><a href="{{asset ('forum') }}">Forum</a></li>
-                        <li><a href="{{asset ('about') }}">About</a></li>
+                        <!--                        <li><a href="{{asset ('about') }}">About</a></li>
+                                                <li><a href="{{asset ('contact') }}">Contact</a></li>-->
 
-          <!-- <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
-            <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
-            </ul>
-          </li> -->
-                        <li><a href="{{asset ('contact') }}">Contact</a></li>
+<!--                                  <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+  <ul>
+    <li><a href="#">Drop Down 1</a></li>
+    <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+      <ul>
+        <li><a href="#">Deep Drop Down 1</a></li>
+        <li><a href="#">Deep Drop Down 2</a></li>
+        <li><a href="#">Deep Drop Down 3</a></li>
+        <li><a href="#">Deep Drop Down 4</a></li>
+        <li><a href="#">Deep Drop Down 5</a></li>
+      </ul>
+    </li>
+    <li><a href="#">Drop Down 2</a></li>
+    <li><a href="#">Drop Down 3</a></li>
+    <li><a href="#">Drop Down 4</a></li>
+  </ul>
+</li>-->
+
                     </ul>
                     <i class="bi bi-list mobile-nav-toggle"></i>
                 </nav><!-- .navbar -->
