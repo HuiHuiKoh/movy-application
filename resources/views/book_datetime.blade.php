8ce6f02e4c4f135bed105a374aa658eb69f2c950
HuiHuiKoh <kohhh-wm19@student.tarc.edu.my>
2022-11-20 19:54:50 +0800

    book_datetime.blade.php

--- resources/views/book_datetime.blade.php
+++ resources/views/book_datetime.blade.php
@@ -4,41 +4,35 @@
 <section id="homepage" class="pricing">
     <div class="container">
         <div>
-            <button type="button" class="btn orange-outline-btn px-5 py-1 mx-2">
-                <div class="text-uppercase">Today</div>
-                <div class="text-uppercase">20-nov</div>
+            <button type="button" class="booking-date btn orange-outline-btn px-5 py-1 mx-2 text-uppercase">
+                <div>Today</div><div>20-nov</div>
             </button>
-            <button type="button" class="btn orange-outline-btn px-5 py-1 mx-2">
-                <div class="text-uppercase">Sun</div>
-                <div class="text-uppercase">21-nov</div>
+            <button type="button" class="booking-date btn orange-outline-btn px-5 py-1 mx-2 text-uppercase">
+                <div>Sun</div><div>21-nov</div>
             </button>
-            <button type="button" class="btn orange-outline-btn px-5 py-1 mx-2">
-                <div class="text-uppercase">Mon</div>
-                <div class="text-uppercase">22-nov</div>
+            <button type="button" class="booking-date btn orange-outline-btn px-5 py-1 mx-2 text-uppercase">
+                <div>Mon</div><div>22-nov</div>
             </button>
         </div>
-        <p>
-            <button
-                class="btn btn-primary"
-                type="button"
-                data-mdb-toggle="collapse"
-                data-mdb-target="#collapseWidthExample"
-                aria-expanded="false"
-                aria-controls="collapseWidthExample"
-                >
-                Toggle width collapse
-            </button>
-        </p>
-        <div style="min-height: 120px;">
-            <div class="collapse collapse-horizontal" id="collapseWidthExample" >
-                <div style="width: 300px;">
-                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad
-                    squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt
-                    sapiente ea proident.
-                </div>
-            </div>
+        <div>
+            <h2 class="font-weight-bold">Movy Bukit Tinggi</h2>
+            <h2 class="font-weight-bold">Movy 1 Utama</h2>
+            <h2 class="font-weight-bold">Movy Rawang</h2>
         </div>
-
     </div>
 </section>
 @endsection
+
+@push('scripts')
+<script>
+//    Make training programme button active
+    let dates = document.querySelectorAll(".booking-date");
+    for (let date of dates) {
+        date.addEventListener('click', (e) => {
+            const et = e.target;
+            $('.booking-date').parent().find('.orange-btn-active').removeClass('orange-btn-active');
+            et.classList.add('orange-btn-active');
+        });
+    }
+</script>
+@endpush
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,7 +27,7 @@
 
         <!--Bootstrap CSS-->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-        
+
         <!-- Template Main CSS File -->
         <link href="{{asset ('import/assets/css/style.css') }}" rel="stylesheet">
         @stack('css')
@@ -107,7 +107,6 @@
                         <div class="logo col-lg-3 col-md-6">
                             <h3>MOVY</h3>
                         </div>
-                        <!--<h1 class="logo me-auto"><a href="{{ asset('home') }}">MOVY</a></h1>-->
 
                         <div class="col-lg-2 col-md-6 footer-links">
                             <h4>Site Links</h4>
@@ -130,26 +129,13 @@
                     </div>
                 </div>
             </div>
-            <!--            
-                        <hr />
-            
-                        <div class="container d-md-flex py-4">
-            
-                            <div class="social-links text-center text-md-right pt-3 pt-md-0">
-                                <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-                                <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-                                <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-                                <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-                                <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-                            </div>
-                        </div>-->
-        </footer><!-- End Footer -->
+        </footer>
 
         <!--<div id="preloader"></div>-->
         <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
         <!-- Vendor JS Files -->
-        @stack('script')
+        @stack('scripts')
         <script src="{{asset ('import/assets/vendor/purecounter/purecounter_vanilla.js') }}"></script>
         <script src="{{asset ('import/assets/vendor/aos/aos.js') }}"></script>
         <script src="{{asset ('import/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
@@ -158,6 +144,9 @@
 
         <!-- Template Main JS File -->
         <script src="{{asset ('import/assets/js/main.js') }}"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
 
     </body>
