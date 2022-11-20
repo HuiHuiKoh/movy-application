e08b0f248c66d7c3fb30c4151ed68d4a660352af
HuiHuiKoh <kohhh-wm19@student.tarc.edu.my>
2022-11-20 18:51:20 +0800

    composer update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,6 +25,9 @@
         <link href="{{asset ('import/assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
         <link href="{{asset ('import/assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
 
+        <!--Bootstrap CSS-->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        
         <!-- Template Main CSS File -->
         <link href="{{asset ('import/assets/css/style.css') }}" rel="stylesheet">
         @stack('css')
