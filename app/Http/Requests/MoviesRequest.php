8902f26e90fcfb93392afc6b595b9222d5edd586
SiahXinYing <66939943+SiahXinYing@users.noreply.github.com>
2022-11-22 09:46:59 +0800

    dimensions

--- app/Http/Requests/MoviesRequest.php
+++ app/Http/Requests/MoviesRequest.php
@@ -14,8 +14,8 @@ class MoviesRequest extends FormRequest {
         return [
             'name' => 'required|string|max:250',
             'synopsis' => 'required|string',
-            'image' => 'required|image|mimes:jpg,png,jpeg,gif,svg|max:2048|'
-            . 'dimensions:min_width=100,min_height=100,max_width=1000,max_height=1000',
+            'image' => 'required|image|mimes:jpg,png,jpeg,gif,svg|max:2048|',
+//            . 'dimensions:min_width=100,min_height=100,max_width=1000,max_height=1000',
             'casts' => 'required|string|max:400',
             'language' => 'required|string|max:20|min:3',
             'type' => 'required|string|max:100',
