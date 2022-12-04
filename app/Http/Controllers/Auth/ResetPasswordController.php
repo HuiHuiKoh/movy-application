e14437f76c8032e0126f74c6d438c8c50069b9f5
SiahXinYing <66939943+SiahXinYing@users.noreply.github.com>
2022-12-04 22:36:53 +0800

    reset password

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use App\Models\User;
 
 class ResetPasswordController extends Controller
 {
@@ -27,4 +28,17 @@ class ResetPasswordController extends Controller
      * @var string
      */
     protected $redirectTo = RouteServiceProvider::HOME;
+    protected function redirectTo(){
+        
+        if(Auth()->user()->role == 1){
+            
+            return route(admin.adminDashboard);
+            
+        }else if(Auth()->user()->role == 0){
+            
+            return route(home);
+        }
+        
+        
+    }
 }
--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,45 @@
-@extends('layouts.app')
+<link rel="stylesheet" href="{{  asset('assets\css\bootstrap.css') }}">
+<link rel="stylesheet" type="text/css" href="{{asset('assets\css\email.css')}}">
+<title>MOVY</title>
+<body class="my-login-page">
+    <section class="h-100">
+        <div class="container h-100">
+            <div class="row justify-content-md-center align-items-center h-100">
+                <div class="card-wrapper">
 
-@section('content')
-<div class="container" style="margin-top: 3%">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+                    <div class="cardx fat">
+                        <div class="card-body">
+                            <h4 class="card-title">Forgot Password</h4>
+                            <form method="POST" class="my-login-validation" novalidate="" action="{{ route('password.email') }}">
+                                @csrf
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                                @if (session('status'))
+                                <div class="alert alert-ssuccess">
+                                    {{ session('status') }}
+                                </div>
+                                @endif
+                                <div class="form-group">
+                                    <label for="email" style="color:#ffffff;">E-Mail Address</label>
+                                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="Enter your email">
+                                    <span class="text-danger">@error('email'){{ $message }} @enderror</span>
+                                </div>
+
+                                <div class="form-group m-0">
+                                    <button type="submit" class="btn btn-primary btn-block">
+                                        Submit
+                                    </button>
+                                </div>
+                            </form>
                         </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary" style="background: #EAD3A3; border-color: #EAD3A3;">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    </div>
+                    <div class="footer">
+                        Copyright &copy; 2022 &mdash; MOVY
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
-@endsection
+    </section>
+
+
+</body>
+
