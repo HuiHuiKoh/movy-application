3902668601e80eb10799b2ab51f9b15402d0a1be
Chloe <kohhh-wm19@student.tarc.edu.my>
2022-12-08 23:05:43 +0800

    membership seeding

--- database/seeders/PromotionSeeder.php
+++ database/seeders/PromotionSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 
-class SeatTypeSeeder extends Seeder {
+class PromotionSeeder extends Seeder {
 
     /**
      * Run the database seeds.
@@ -14,14 +14,42 @@ class SeatTypeSeeder extends Seeder {
      * @return void
      */
     public function run() {
-        DB::table('seat_types')->insert([
+        DB::table('promotions')->insert([
             [
                 'id' => 1,
-                'title' => ,
-                'description' => ,
-                'image' => ,
-                'created_at' => Carbon::now()->toDateTimeString(),
+                'title' => 'Avatar: The Way Of Water Exclusive Posters Promo',
+                'description' => 'Purchase movie tickets at any selected participating cinema through MOVY online platforms and counter.',
+                'image' => 'promo_avatar_poster.jpg',
+                'created_at' => Carbon::now()->toDateTimeString()
             ],
+            [
+                'id' => 2,
+                'title' => 'New Flavour: Cajun Coney Promo',
+                'description' => 'Cajun Coney with Chicken Cajun Sauce with combo or ala carte',
+                'image' => 'promo_cajun.png',
+                'created_at' => Carbon::now()->toDateTimeString()
+            ],
+            [
+                'id' => 3,
+                'title' => 'Fried Mac and Cheese Pops Promo',
+                'description' => 'Fried Mac and Cheese Pops with combo or ala carte',
+                'image' => 'promo_cheese.png',
+                'created_at' => Carbon::now()->toDateTimeString()
+            ],
+            [
+                'id' => 4,
+                'title' => 'Christmas Sale 2022: Up to 50% OFF',
+                'description' => 'Celebrate Christmas with MOVY',
+                'image' => 'promo_christmas_sales.png',
+                'created_at' => Carbon::now()->toDateTimeString()
+            ],
+            [
+                'id' => 5,
+                'title' => 'New Flavour: Salted Caramel Lemon Popcorn',
+                'description' => 'Purchase movie tickets with foods at any selected participating cinema through MOVY online platforms or counter',
+                'image' => 'promo_popcorn.png',
+                'created_at' => Carbon::now()->toDateTimeString()
+            ]
         ]);
     }
 
--- database/seeders/VoucherSeeder.php
+++ database/seeders/VoucherSeeder.php
@@ -3,16 +3,51 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 
-class VoucherSeeder extends Seeder
-{
+class VoucherSeeder extends Seeder {
+
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
-        //
+    public function run() {
+        DB::table('vouchers')->insert([
+            [
+                'id' => 1,
+                'title' => "Save RM18 OFF Movy New User Voucher Code On Anything",
+                'code' => 'VC18',
+                'discount_amount' => 18.00,
+                'exp_date' => '2023-01-31',
+                'created_at' => Carbon::now()->toDateTimeString(),
+            ],
+            [
+                'id' => 2,
+                'title' => "Save RM10 OFF Movy Promo Code Malaysia On Movie Ticket Purchase",
+                'code' => 'VC10',
+                'discount_amount' => 10.00,
+                'exp_date' => '2023-02-28',
+                'created_at' => Carbon::now()->toDateTimeString(),
+            ],
+            [
+                'id' => 3,
+                'title' => "Grab Up To 50% OFF Christmas Deals",
+                'code' => 'VC50P',
+                'discount_amount' => 50.00,
+                'exp_date' => '2023-12-31',
+                'created_at' => Carbon::now()->toDateTimeString(),
+            ],
+            [
+                'id' => 4,
+                'title' => "Get 35% OFF Movy Promo Code On Food Purchase",
+                'code' => 'VC35P',
+                'discount_amount' => 35.00,
+                'exp_date' => '2023-01-31',
+                'created_at' => Carbon::now()->toDateTimeString(),
+            ],
+        ]);
     }
+
 }
